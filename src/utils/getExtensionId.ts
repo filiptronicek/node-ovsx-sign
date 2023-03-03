c3503ab728e2da8d7cd273f1d478916a11058b39
Filip Troníček <filip.tronicek@seznam.cz>
2023-03-03 17:28:21 +0100

    Template string

--- src/utils/getExtensionId.ts
+++ src/utils/getExtensionId.ts
@@ -2,5 +2,5 @@ import { readVSIXPackage } from '@vscode/vsce/out/zip';
 
 export const getExtensionId = async (vsixPath: string): Promise<string> => {
     const { manifest } = await readVSIXPackage(vsixPath);
-    return manifest.publisher + '.' + manifest.name;
+    return `${manifest.publisher}.${manifest.name}`;
 };
