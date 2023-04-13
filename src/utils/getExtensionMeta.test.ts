4e1e597885ab7bd6670381050026c4ccf515977d
Filip Troníček <filip.tronicek@seznam.cz>
2023-04-13 16:01:20 +0200

    Silence, `tsc`!

--- src/utils/getExtensionMeta.test.ts
+++ src/utils/getExtensionMeta.test.ts
@@ -1,6 +1,6 @@
 import { download } from './download';
 import { getExtensionMeta } from './getExtensionMeta';
-import { unlink } from 'fs/promises'
+import { unlink } from 'fs/promises';
 
 jest.setTimeout(20_000);
 
@@ -8,7 +8,6 @@ describe('extensionTest', () => {
   test('be able to verify an extension', async () => {
     const extensionDestination = await download("https://open-vsx.org/api/jeanp413/open-remote-ssh/0.0.33/file/jeanp413.open-remote-ssh-0.0.33.vsix", {});
     const metadata = await getExtensionMeta(extensionDestination);
-    console.table(metadata)
     expect(metadata.id).toBe('jeanp413.open-remote-ssh');
     expect(metadata.version).toBe('0.0.33');
 
