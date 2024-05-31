f6a0ecf6e98df35a07c29ff3fda3f9d4ff9e1d5b
Filip Troníček <filip@gitpod.io>
2024-05-31 12:42:43 +0200

    eslint fixes

--- src/utils/signature-manifest.test.ts
+++ src/utils/signature-manifest.test.ts
@@ -8,7 +8,7 @@ jest.setTimeout(20_000);
 describe("signatureManifestTest", () => {
     test("be able to generate a signature manifest file", async () => {
         const extensionDestination = await download("https://ms-python.gallerycdn.vsassets.io/extensions/ms-python/python/2024.7.11511013/1717064437177/Microsoft.VisualStudio.Services.VSIXPackage", {});
-        const signatureArchivePath = await download("https://ms-python.gallerycdn.vsassets.io/extensions/ms-python/python/2024.7.11511013/1717064437177/Microsoft.VisualStudio.Services.VsixSignature", {filename: "signature.sigzip"});
+        const signatureArchivePath = await download("https://ms-python.gallerycdn.vsassets.io/extensions/ms-python/python/2024.7.11511013/1717064437177/Microsoft.VisualStudio.Services.VsixSignature", { filename: "signature.sigzip" });
         const expectedSignatureManifest = JSON.parse((await extractFileAsBufferUsingStreams(signatureArchivePath, ".signature.manifest")).toString("utf-8"));
 
         const generatedManifest = await generateSignatureManifest(extensionDestination);
